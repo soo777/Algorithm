26d3af1d8c93a3b7cab8405227537dcd1052dce5
soo777 <ssm19944@gmail.com>
2021-03-21 21:46:18 +0900

    구현 - N2741

---
+++ src/Implement/N2741/Main.java
@@ -0,0 +1,19 @@
+package Implement.N2741;
+
+// 백준
+// 2741
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input;
+        input = scanner.nextInt();
+
+        for(int i=1; i<input+1; i++){
+            System.out.println(i);
+        }
+    }
+}
