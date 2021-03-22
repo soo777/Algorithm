aa597bdedc62bd7db5d4d70c4e776d90d267afb8
soo777 <ssm19944@gmail.com>
2021-03-22 19:52:27 +0900

    구현 - N15552 / 빠른 A+B

---
+++ src/Implement/N15552/Main.java
@@ -0,0 +1,28 @@
+package Implement.N15552;
+
+// 백준
+// 15552 빠른 A+B
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int input1 = Integer.parseInt(st.nextToken());
+            int input2 = Integer.parseInt(st.nextToken());
+            bw.write(input1 + input2 + "\n");
+            bw.write(i);
+        }
+
+        bw.close();
+    }
+}
