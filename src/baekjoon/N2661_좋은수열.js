831e45a9db08e938db88ee31abe0a73b15b74730
soo777 <ssm19944@gmail.com>
2021-07-01 22:19:49 +0900

    백준 - N2661 / 좋은수열 (2)

--- src/baekjoon/N2661_좋은수열.js
+++ src/baekjoon/N2661_좋은수열.js
@@ -8,44 +8,63 @@
   4. 같은 문자열이 나오면 수열 사용 불가능 
   5. 같은 문자열이 발견되지 않으면 -> 좋은 수열 -> 다음 재귀 호출
   6. n 자리 수열을 구하는 경우 종료 (가장 먼저 구한 수열이므로 -> 가장 작은 수 -> 다음 수열 구할 필요 없음)
+
+  디버깅은 통과하는데 백준 제출은 통과못함.
 */
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-let input = [];
+// let input = [];
+// let check = false;
+
+// rl.on('line', function (line) {
+//   input.push(line);
+// })
+//   .on('close', function () {
+//     n = input[0];
+
+//     dfs(2, "1");
+
+//     process.exit();
+//   });
+
+let n = 7;
 let check = false;
-
-rl.on('line', function (line) {
-  input.push(line);
-})
-  .on('close', function () {
-    n = input[0];
-
-    dfs(1, "1");
-
-    process.exit();
-  });
-
 
 const dfs = (len, sequence) => {
   if (check) {
     return;
   }
 
-  if (n === len) {
+  if (len === n + 1) {
     console.log(sequence);
     check = true;
     return;
   }
 
   for (let i = 1; i <= 3; i++) {
-    sequence = sequence + i;
+    let next = sequence + i;
+    let tempCheck = true;
 
+    for (let i = 1; i <= len / 2; i++) {
+      let q = next.substring(len - i, len);
+      let w = next.substring(len - i - i, len - i);
+
+      if (q === w) {
+        tempCheck = false;
+        break;
+      }
+    }
+    if (!tempCheck) {
+      continue;
+    } else {
+      dfs(len + 1, next);
+    }
   }
-
-  return;
 }
+
+dfs(2, "1");
